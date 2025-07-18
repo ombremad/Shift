23abe3e59e879f1db5afc80ea8a20a27bae35d2d
Mehdi <legoullonmehdi@gmail.com>
2025-07-18 14:50:29 +0200

    Add Event model and start Event page

--- Shift/Features/Events/EventsView.swift
+++ Shift/Features/Events/EventsView.swift
@@ -8,8 +8,28 @@
 import SwiftUI
 
 struct EventsView: View {
+    
+    @State private var textInput: String = "";
+
     var body: some View {
-        Text("Events")
+        VStack(alignment: .leading) {
+            Text("Events")
+                .font(.custom("Safiro-Bold", size: 36))
+
+            HStack {
+                TextField("Search", text: $textInput)
+                    .frame(width: 316, height: 40)
+                    .background(
+                        RoundedRectangle(cornerRadius: 10)
+                            .stroke(Color.gray, lineWidth: 1)
+                    )
+                Spacer()
+            }
+
+            Spacer()
+        }
+        .padding()
+        .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .leading)
     }
 }
 
