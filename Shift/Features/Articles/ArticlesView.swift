1489553feba63be643f081cda18003cb2f43e373
Alize Nom <alizesuchon.com>
2025-07-22 12:12:18 +0200

    Ajout avant mise en commun

--- Shift/Features/Articles/ArticlesView.swift
+++ Shift/Features/Articles/ArticlesView.swift
@@ -12,25 +12,27 @@ struct ArticlesView: View {
     @State private var viewModel = ArticlesViewModel()
     
     var body: some View {
-        VStack (alignment: .leading){
-            Text("What's new ?")
-                .font(.custom("Safiro-Bold", size: 36))
-//            HStack (spacing: 16){
-//            ForEach(SliderView){ NewArticle in
-//                    
-//                }
-           // }
-            Text("Latest")
-                .font(.custom("Safiro-Medium", size: 24))
-          
+        NavigationStack {
+            VStack (alignment: .leading){
+                Text("What's new ?")
+                    .font(.custom("Safiro-Bold", size: 36))
+                ScrollView(.horizontal, showsIndicators: false) {
+                    HStack(spacing: 16) {
+                        ForEach(viewModel.articlesArray) { article in
+                            SliderView(article: article)
+                        }
+                    }
+                }
+                Text("Latest")
+                    .font(.custom("Safiro-Bold", size: 24))
+            }
+            .padding()
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+            .background(Color.background)
+            .ignoresSafeArea()
         }
-        .padding()
-        .frame(maxWidth: .infinity, maxHeight: .infinity)
-               .background(Color.background)
-               .ignoresSafeArea()
     }
 }
-
-#Preview {
-    ArticlesView()
-}
+    #Preview {
+        ArticlesView()
+    }
