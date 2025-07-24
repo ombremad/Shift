d7080cb14388db8e21b8e3ce165db3649b919e25
Mehdi <legoullonmehdi@gmail.com>
2025-07-24 13:05:32 +0200

    Add Event Detail View Page with navigation and clean code

---
+++ Shift/Features/Events/EventDetailView.swift
@@ -0,0 +1,18 @@
+//
+//  EventDetailView.swift
+//  Shift
+//
+//  Created by Mehdi Legoullon on 24/07/2025.
+//
+
+import SwiftUI
+
+struct EventDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    EventDetailView()
+}
--- Shift/Features/Events/EventsView.swift
+++ Shift/Features/Events/EventsView.swift
@@ -31,73 +31,76 @@ struct EventsView: View {
     }
 
     var body: some View {
+        NavigationStack {
+            VStack(alignment: .leading) {
+                Text("Events")
+                    .font(.custom("Safiro-Bold", size: 36))
+                    .padding(.leading, 25)
 
-        VStack(alignment: .leading) {
-            Text("Events")
-                .font(.custom("Safiro-Bold", size: 36))
+                HStack {
+
+                    ZStack(alignment: .leading) {
+                        RoundedRectangle(cornerRadius: 10)
+                            .stroke(Color.gray, lineWidth: 1)
+                            .frame(width: 310, height: 40)
+
+                        Image("magnifying-glass")
+                            .frame(width: 40, height: 40)
+                            .background(Color("NeonGreen"))
+                            .clipShape(RoundedRectangle(cornerRadius: 10))
+
+                        TextField("Search", text: $searchEvent)
+                            .font(.custom("Helvetica Neue", size: 14))
+                            .padding(.leading, 50)
+                            .frame(width: 316, height: 40)
+                    }
+
+                    Button(action: {
+                        showingFilterModal.toggle()
+                    }) {
+                        Image("faders")
+                            .frame(width: 42, height: 40)
+                            .background(Color("NeonGreen"))
+                            .clipShape(RoundedRectangle(cornerRadius: 10))
+                            .foregroundStyle(.black)
+                    }
+
+                }
                 .padding(.leading, 25)
 
-            HStack {
-
-                ZStack(alignment: .leading) {
-                    RoundedRectangle(cornerRadius: 10)
-                        .stroke(Color.gray, lineWidth: 1)
-                        .frame(width: 310, height: 40)
-
-                    Image("magnifying-glass")
-                        .frame(width: 40, height: 40)
-                        .background(Color("NeonGreen"))
-                        .clipShape(RoundedRectangle(cornerRadius: 10))
-
-                    TextField("Search", text: $searchEvent)
-                        .font(.custom("Helvetica Neue", size: 14))
-                        .padding(.leading, 50)
-                        .frame(width: 316, height: 40)
-                }
-
-                Button(action: {
-                    showingFilterModal.toggle()
-                }) {
-                    Image("faders")
-                        .frame(width: 42, height: 40)
-                        .background(Color("NeonGreen"))
-                        .clipShape(RoundedRectangle(cornerRadius: 10))
-                        .foregroundStyle(.black)
-                }
-
-            }
-            .padding(.leading, 25)
-
-            HStack {
-                Picker("", selection: $selectedTab) {
-                    ForEach(0..<tabs.count, id: \.self) { index in
-                        Text(self.tabs[index])
-                            .tag(index)
+                HStack {
+                    Picker("", selection: $selectedTab) {
+                        ForEach(0..<tabs.count, id: \.self) { index in
+                            Text(self.tabs[index])
+                                .tag(index)
+                        }
                     }
+                    .pickerStyle(SegmentedPickerStyle())
+                    .frame(width: 370, height: 43)
+                    .cornerRadius(10)
                 }
-                .pickerStyle(SegmentedPickerStyle())
-                .frame(width: 370, height: 43)
-                .cornerRadius(10)
-            }
-            .padding(.top, 28)
-            .padding(.leading, 12)
+                .padding(.top, 28)
+                .padding(.leading, 12)
 
-            ScrollView {
-                VStack(spacing: 15) {
-                    ForEach(staticData[selectedTab], id: \.self) { item in
-                        CardView(title: item)
+                ScrollView {
+                    VStack(spacing: 15) {
+                        ForEach(staticData[selectedTab], id: \.self) { item in
+                            NavigationLink(destination: EventDetailView()) {
+                                CardView(title: item)
+                            }
+                        }
                     }
+                    .padding(.vertical, 10)
                 }
-                .padding(.vertical, 10)
+                .scrollIndicators(.hidden)
+                .padding()
+                .padding(.top, 19)
             }
-            .scrollIndicators(.hidden)
-            .padding()
-            .padding(.top, 19)
+            .sheet(isPresented: $showingFilterModal) {
+                FilterModalView(showingFilterModal: $showingFilterModal)
+            }
+            .background(Color.background)
         }
-        .sheet(isPresented: $showingFilterModal) {
-            FilterModalView(showingFilterModal: $showingFilterModal)
-        }
-        .background(Color.background)
     }
 }
 
