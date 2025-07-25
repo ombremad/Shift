51a6bae34be49cf557964ff4684160e33c4686b7
“Samara <samara.limads@gmail.com>
2025-07-25 16:45:26 +0200

    created tab bar following MVVM structure

---
+++ Shift/Features/Shared/Views/TabBarView.swift
@@ -0,0 +1,67 @@
+//
+//  TabBarView.swift
+//  Shift
+//
+//  Created by Samara Lima da Silva on 15/07/2025.
+//
+
+import SwiftUI
+
+struct TabBarView: View {
+    @StateObject private var viewModel = TabBarViewModel()
+
+    var body: some View {
+        VStack(spacing: -12) {
+            // MARK: - Main content area
+            ZStack {
+                switch viewModel.selectedTab {
+                case .inspirations:
+                    InspirationsView()
+                case .articles:
+                    ArticlesView()
+                case .dashboard:
+                    DashboardView()
+                case .forum:
+                    ForumView()
+                case .events:
+                    EventsView()
+                }
+            }
+
+            // MARK: - Custom Tab Bar
+            HStack {
+                ForEach(viewModel.tabs, id: \.id) { item in
+                    tabBarItem(item: item)
+                }
+            }
+            .padding(.horizontal, 15)
+            .frame(maxWidth: .infinity, maxHeight: 84)
+            .background(.white.opacity(0.95))
+            .cornerRadius(15)
+            .shadow(color: .black.opacity(0.4), radius: 0.7, x: 0, y: 0)
+        }
+        .edgesIgnoringSafeArea(.bottom)
+    }
+
+    private func tabBarItem(item: TabItem) -> some View {
+        Button(action: {
+            viewModel.selectedTab = item.tab
+        }) {
+            VStack() {
+                Image(item.icon)
+                    .font(.system(size: 23, weight: .regular))
+                    .foregroundColor(viewModel.selectedTab == item.tab ? Color("Violet") : .gray)
+                Text(item.title)
+                    .font(.custom("HelveticaNeue", size: 10))
+                    .foregroundColor(viewModel.selectedTab == item.tab ? Color("Violet") : .gray)
+            }
+            .frame(maxWidth: .infinity)
+            .padding(.top, 10)
+            .padding(.bottom, 34)
+        }
+    }
+}
+
+#Preview {
+    TabBarView()
+}
