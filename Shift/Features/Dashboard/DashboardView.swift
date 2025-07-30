b2e66815468f420c51a95892fccce48cc10a791d
Mehdi <legoullonmehdi@gmail.com>
2025-07-30 12:21:55 +0200

    Clean indent for the Dashboard page

--- Shift/Features/Dashboard/DashboardView.swift
+++ Shift/Features/Dashboard/DashboardView.swift
@@ -12,14 +12,14 @@ struct DashboardView: View {
     @State var forumViewModel = ForumViewModel()
     @State private var articlesViewModel = ArticlesViewModel()
     @State private var eventsViewModel = EventsViewModel()
-    
+
     var body: some View {
         NavigationStack {
             ScrollView {
                 VStack(alignment: .leading) {
-                    
+
                     // MARK: - Welcome User
-                    
+
                     // TODO: - Add logic to recover current user's image and name
                     HStack(spacing: 12) {
                         Image(.profile)
@@ -27,11 +27,11 @@ struct DashboardView: View {
                             .scaledToFill()
                             .frame(width: 52, height: 52)
                             .clipShape(Circle())
-                        
+
                         Text("Welcome Julie")
                             .font(.custom("Safiro-SemiBold", size: 32))
                     }
-                    
+
                     // MARK: - Profile
                     NavigationLink(destination: ProfilView()) {
                         Text("My profile")
@@ -46,16 +46,18 @@ struct DashboardView: View {
                     .padding(.bottom, 23)
                     .padding(.top, -10)
                     .tint(.violet)
-                    
+
                     // MARK: - Daily Inspiration
                     Text("Daily Inspiration")
                         .font(.custom("Safiro-SemiBold", size: 24))
                         .padding(.bottom, 13)
-                    
-                    
+
                     if let woman = viewModel.selectedWoman,
-                       let index = viewModel.women.firstIndex(where: { $0.id == woman.id }) {
-                        
+                        let index = viewModel.women.firstIndex(where: {
+                            $0.id == woman.id
+                        })
+                    {
+
                         NavigationLink(
                             destination: InspirationDetailView(
                                 women: viewModel.women,
@@ -66,34 +68,49 @@ struct DashboardView: View {
                         }
                         .buttonStyle(PlainButtonStyle())
                     }
-                    
+
                     // MARK: - Latest Articles
                     Text("Latest Articles")
                         .font(.custom("Safiro-SemiBold", size: 24))
                         .padding(.top, 20)
-                    VStack{
-                        ForEach(articlesViewModel.sortedByDate(array: articlesViewModel.articlesArray).prefix(2)) { article in
-                            NavigationLink(destination : DetailArticleView(
-                                article: article
-                            ).environment(articlesViewModel)) {
-                                LittleArticlesView(article : article, fromDashboard: true)
+                    VStack {
+                        ForEach(
+                            articlesViewModel.sortedByDate(
+                                array: articlesViewModel.articlesArray
+                            ).prefix(2)
+                        ) { article in
+                            NavigationLink(
+                                destination: DetailArticleView(
+                                    article: article
+                                ).environment(articlesViewModel)
+                            ) {
+                                LittleArticlesView(
+                                    article: article,
+                                    fromDashboard: true
+                                )
                             }
                         }
                     }
-                    
-                     // MARK: - Hot Topics
+
+                    // MARK: - Hot Topics
                     Text("Hot Topics")
                         .font(.custom("Safiro-SemiBold", size: 24))
                     HotTopicsView()
-                  
+
                     // MARK: - Events
                     Text("Popular events")
                         .font(.custom("Safiro-SemiBold", size: 24))
-                    
+
                     ScrollView(.horizontal, showsIndicators: false) {
                         HStack(spacing: 10) {
-                            ForEach(eventsViewModel.displayedEvents, id: \.id) { event in
-                                NavigationLink(destination: EventDetailView(event: event, viewModel: eventsViewModel)) {
+                            ForEach(eventsViewModel.displayedEvents, id: \.id) {
+                                event in
+                                NavigationLink(
+                                    destination: EventDetailView(
+                                        event: event,
+                                        viewModel: eventsViewModel
+                                    )
+                                ) {
                                     DashboardEventCard(event: event)
                                         .padding(.trailing, 15)
                                 }
@@ -105,14 +122,15 @@ struct DashboardView: View {
                 .padding(.top)
                 .padding(.leading, 20)
             }
-            .background(Color("Background")
-                .ignoresSafeArea())
-            
+            .background(
+                Color("Background")
+                    .ignoresSafeArea()
+            )
+
         }
         .tint(.noir)
     }
 }
-
 
 #Preview {
     DashboardView()
